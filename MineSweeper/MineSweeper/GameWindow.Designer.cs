b3e67f9960c583ccdb30945af95442b51296c3aa
hungryglobe8 <casrand@gmail.com>
2020-08-09 23:47:46 -0600

    Form can now generate a basic minesweeper field and displays numbers indicating danger. Shows a bomb if tile is armed. Need to play around with icon 16x16 perfect size!

--- MineSweeper/MineSweeper/GameWindow.Designer.cs
+++ MineSweeper/MineSweeper/GameWindow.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // GameWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(719, 651);
+            this.Name = "GameWindow";
+            this.Text = "MineSweeper";
+            this.Load += new System.EventHandler(this.GameWindow_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- MineSweeper/MineSweeper/GameWindow.cs
+++ MineSweeper/MineSweeper/GameWindow.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Engine;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,46 @@ namespace MineSweeper
         public GameWindow()
         {
             InitializeComponent();
+            Field field = new Field(4, 4, 6);
+            field.PopulateField(10);
+            CreateBoard(field);
+        }
+
+        private void GameWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreateBoard(Field field)
+        {
+            int BoardWidth = 4;
+            int BoardHeight = 4;
+            int BUTTON_SIZE = 20;
+            for (int x = 0; x < BoardWidth; x++)
+            {
+                for (int y = 0; y < BoardHeight; y++)
+                {
+                    Button b = new Button();
+                    b.Click += PieceButton_Click;
+                    b.Location = new Point(BUTTON_SIZE * (x + 1), BUTTON_SIZE * (y + 1));
+                    b.Size = new Size(BUTTON_SIZE, BUTTON_SIZE);
+                    
+                    Tile tile = field.GetTile(x, y);
+                    //bomb image
+                    if (tile.IsArmed)
+                        b.Image = Image.FromFile("../../Images/Icon1.ico");
+                    //num surrounding mines
+                    else
+                        b.Text = tile.GetDanger().ToString();
+
+                    Controls.Add(b);
+                }
+            }
+        }
+        private void PieceButton_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            MessageBox.Show(button.Location.X + "/" + button.Location.Y);
         }
     }
 }
