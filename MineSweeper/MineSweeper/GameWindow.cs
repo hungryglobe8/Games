a069fa8a05a7e413a0fb117df61cfa9822ca368d
hungryglobe8 <casrand@gmail.com>
2020-08-12 21:44:02 -0600

    Delete extra

--- MineSweeper/MineSweeper/GameWindow.cs
+++ MineSweeper/MineSweeper/GameWindow.cs
@@ -39,17 +39,9 @@ namespace MineSweeper
                     Tile tile = field.GetTile(x, y);
                     Button b = tile.button;
                     b.MouseUp += (sender, e) => Button_MouseUp(sender, e, tile);
-                    //b.MouseUp += (sender, e) => Tile_MouseUp(sender, e, tile);
                     //b.EnabledChanged += Tile_EnableChanged;
                     b.Location = new Point(BUTTON_SIZE * (x + 1), BUTTON_SIZE * (y + 1));
                     b.Size = new Size(BUTTON_SIZE, BUTTON_SIZE);
-                    
-                    //bomb image
-                    //if (tile.IsArmed)
-                    //    b.Image = Image.FromFile("../../Images/Bomb.bmp");
-                    ////num surrounding mines
-                    //else
-                    //    b.Text = tile.GetDanger().ToString();
 
                     Controls.Add(b);
                 }
@@ -80,70 +72,6 @@ namespace MineSweeper
             var button = (Button)sender;
             button.ForeColor = button.Enabled == false ? Color.Blue : Color.Red;
             button.BackColor = Color.AliceBlue;
-        }
-
-        //private void Tile_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e, Tile tile)
-        //{
-        //    var clickedButton = (Button)sender;
-        //    string res = tile.Click(e);
-        //    if (res == "Bomb")
-        //        clickedButton.Image = Image.FromFile("../../Images/Bomb.bmp");
-
-        //    // Reveal state.
-        //    if (clickedButton.Enabled)
-        //    {
-        //        // Multi click on revealed square reveals neighbors.
-        //        // TODO: incorporate into MouseDown event.
-        //        //if (e.Button == MouseButtons.Left && e.Button == MouseButtons.Right)
-        //        //{
-        //        //    MessageBox.Show("Double click used properly!");
-        //        //}
-
-        //        if (e.Button == MouseButtons.Left && !tile.IsFlagged)
-        //        {
-        //            //bomb
-        //            if (tile.IsArmed)
-        //            {
-        //                clickedButton.Image = Image.FromFile("../../Images/Bomb.bmp");
-        //                //GameOver();
-        //            }
-        //            //normal
-        //            else
-        //            {
-        //                var colors = new Dictionary<int, Color>(){
-        //                    {1, Color.Blue },
-        //                    {2, Color.Green },
-        //                    {3, Color.OrangeRed },
-        //                    {4, Color.BlueViolet },
-        //                    {5, Color.Brown },
-        //                    {6, Color.Teal }
-        //                };
-        //                int danger = tile.GetDanger();
-        //                if (danger != 0)
-        //                {
-        //                    clickedButton.Text = danger.ToString();
-        //                    clickedButton.ForeColor = colors[danger];
-        //                }
-        //            }
-
-        //            //deactivate
-        //            //clickedButton.Enabled = false;
-        //        }
-        //        // Flag or deflag tile.
-        //        else if (e.Button == MouseButtons.Right)
-        //        {
-        //            if (!tile.IsFlagged)
-        //            {
-        //                clickedButton.Image = Image.FromFile("../../Images/Flag.bmp");
-        //                tile.IsFlagged = true;
-        //            }
-        //            else
-        //            {
-        //                clickedButton.Image = null;
-        //                tile.IsFlagged = false;
-        //            }
-        //        } 
-        //    }   
-        //}
+        }  
     }
 }
